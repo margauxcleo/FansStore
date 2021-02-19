4c03e695496617a3156c9c2ab23e1473dbc28b8c
Margaux Vitez <margaux.vitez@hotmail.fr>
2021-02-19 22:12:56 +0100

    [MV]V1.3[1] ajout CDN Bootstrap et Fontawesome

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import Universes from './components/Universes/Universes';
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
