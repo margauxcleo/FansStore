12d5e6e60c7f5fcea44e7508e985a3a51b5e0687
Margaux Vitez <margaux.vitez@hotmail.fr>
2021-02-24 17:15:53 +0100

    [MV]V1.3[1,3,4] initialisation de redux pour titre page

---
+++ client/src/actions/index.js
@@ -0,0 +1,48 @@
+// on met un export devant chaque action pour pouvoir les récupérer ailleurs 
+export const setHarryPotter = () => {
+    return {
+      // on décrit l'action
+      // une action est toujours en majuscule 
+      type: 'HARRY_POTTER'
+    };
+};
+
+export const setMarvel = () => {
+  return {
+    // on décrit l'action
+    // une action est toujours en majuscule 
+    type: 'MARVEL'
+  };
+};
+
+export const setStarWars = () => {
+  return {
+    // on décrit l'action
+    // une action est toujours en majuscule 
+    type: 'SW'
+  };
+};
+
+export const setSda = () => {
+  return {
+    // on décrit l'action
+    // une action est toujours en majuscule 
+    type: 'SDA'
+  };
+};
+
+export const setCollections = () => {
+  return {
+    // on décrit l'action
+    // une action est toujours en majuscule 
+    type: 'COLLECTIONS'
+  };
+};
+
+export const setUnivers = () => {
+  return {
+    // on décrit l'action
+    // une action est toujours en majuscule 
+    type: 'UNIVERS'
+  };
+};
