e52ffc7619dc128c9b0c35d43119d2b6ac36df39
Margaux Vitez <margaux.vitez@hotmail.fr>
2021-02-27 19:11:47 +0100

    [MV]V.1.4 mise en commentaire du store de Redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,26 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// V.1.4 pour le moment, on met le store de Redux en stand-by parce qu'on ne s'en sert pas
 // combineReducers : permet de combiner plusieurs reducers 
-import { createStore } from 'redux';
-import { Provider } from "react-redux";
+// import { createStore } from 'redux';
+// import { Provider } from "react-redux";
 
-import reducers from "./reducers";
-// par défaut, react ira dans le fichier index.js du dossier reducers
-// on créé ensuite notre store avec ce gros reducer
-const store = createStore(
-  reducers,
-  // on ajoute cette ligne de code (github react redux) pour que les infos s'affichent dans la console 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// import reducers from "./reducers";
+// // par défaut, react ira dans le fichier index.js du dossier reducers
+// // on créé ensuite notre store avec ce gros reducer
+// const store = createStore(
+//   reducers,
+//   // on ajoute cette ligne de code (github react redux) pour que les infos s'affichent dans la console 
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 
 ReactDOM.render(
   <React.StrictMode>
     {/* permet de lier le store à App */}
     {/* toute modification d'un state contenu dans store, va update le render */}
-    <Provider store={store}>
+    {/* <Provider store={store}>
       <App />
-    </Provider>
+    </Provider> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,14 +1,14 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 
-// on importe tout depuis ce nouveau fichier index.js 
-// par défaut, quand on aura besoin de créer le store, on indiquera le nom du dossier reducer
-// index étant un nom de fichier réservé, react ira automotiquement ici chercher le reducers
-import pageTitleReducer from './pageTitleReducer';
-// import isLoggedReducer from './isLoggedReducer';
+// // on importe tout depuis ce nouveau fichier index.js 
+// // par défaut, quand on aura besoin de créer le store, on indiquera le nom du dossier reducer
+// // index étant un nom de fichier réservé, react ira automotiquement ici chercher le reducers
+// import pageTitleReducer from './pageTitleReducer';
+// // import isLoggedReducer from './isLoggedReducer';
 
-// on combine nos reducers 
-// combineReducers() prend toujours un objet en paramètre 
-const reducers = combineReducers({ pageTitleReducer });
+// // on combine nos reducers 
+// // combineReducers() prend toujours un objet en paramètre 
+// const reducers = combineReducers({ pageTitleReducer });
 
-// ensuite, on exporte ce "gros" reducer
-export default reducers;
+// // ensuite, on exporte ce "gros" reducer
+// export default reducers;
