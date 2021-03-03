c447ee98f454b0d72ced3fcdc5a97fc1f033b3e4
Senade Loko <lokosenade@gmail.com>
2021-03-04 00:16:36 +0100

    [SL] commit de la page inscription

--- client/src/components/Clients/SignUp.js
+++ client/src/components/Clients/SignUp.js
@@ -1,57 +1,64 @@
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import Col from 'react-bootstrap/Col';
+
 import SignUpContent from './SignUpContent.js';
 import {useForm} from 'react-hook-form';
 import { useState } from "react";
-import {yupResolver} from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import './content.css';
 
-const schema = yup.object().shape({
-  email: yup.string().required(<div className="alert alert-danger" role="alert">Entrez votre email svp</div>).email(),
-  mdp: yup.string().required(<div className="alert alert-danger" role="alert">Entrez votre mot de passe svp</div>).matches(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,15}$/,
-      "le mot de passe doit contenir minimum 8 caractères, 1 majuscule, 1 minuscule, 1 nombre et 1 caractère spécial"
-    ),
-  mdpcheck: yup.string().required(<div className="alert alert-danger" role="alert">Entrez votre mot de passe svp</div>).matches(
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,15}$/,
-        "le mot de passe doit contenir minimum 8 caractères, 1 majuscule, 1 minuscule, 1 nombre et 1 caractère spécial",
-      ),
-  prenom: yup.string().required(<div className="alert alert-danger" role="alert">Entrez votre prénom svp</div>).min(3),
-  nom: yup.string().required(<div className="alert alert-danger" role="alert">Entrez votre nom svp</div>).min(3),
-  datenaissance: yup.date().required(<div className="alert alert-danger" role="alert">Entrez une date svp</div>),
-})
 
-const SignUp = (props) => {
-  const {register, handleSubmit, errors} = useForm({
-    resolver: yupResolver(schema),
-  });
 
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+
+const SignUp = () => {
+ const onSubmit = data => {
+     console.log(data);
+ };
+ const {register, handleSubmit, errors} = useForm();
+
 return (
     <div>
-      <h1>Inscription</h1>
+      <h1>Informations nécessaires</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {SignUpContent.inputs.map((input, key)=>{
-          return(
-            <div key={key}>
-            <p>
-              <label>{input.label}</label>
-            </p>
-            <p>
-              <input name={input.name} className="input" type={input.type} ref={register} />
-            </p>
-            <p className="messages">{errors[input.name]?.message}</p>
-          </div>
-          )
-        })}
-      <div className="form-group">
+        <div className="row mb-4">
+            <div className="col">
+                <input name="prenom" type="text" placeholder="prénom" ref={register({required: true, minLength: 3})} required/>
+                {errors.prenom && <div className="alert alert-danger" role="alert">le prénom doit faire plus de 3 caractères</div>}
+            </div>
+            <div className="col">
+                <input name="nom" type="text" placeholder="nom" ref={register({required: true, minLength: 3})} required/>
+                {errors.nom && <div className="alert alert-danger" role="alert">le nom doit faire plus de 3 caractères</div>}
+            </div>
+        </div>
+        <div className="row mb-4">
+            <div className="col">
+                <input name="email" type="email" placeholder="adresse email" ref={register({required: true, minLength: 8})} required/>
+                {errors.email && <div className="alert alert-danger" role="alert">l'email doit faire plus de 8 caractères</div>}
+            </div>
+            <div className="col">
+                <input name="date" type="date" placeholder="date" ref={register({required: true})}/>
+                {errors.date && <div className="alert alert-danger" role="alert">la date ne doit pas être vide</div>}
+            </div>
+        </div>
+        <div className="row mb-4">
+            <div className="col">
+                <input name="password" type="password" placeholder="mot de passe" ref={register({required: true, minLength: 8, pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,15}$/})}/>
+                {errors.password && <div className="alert alert-danger" role="alert">le mot de passe doit contenir minimum 8 caractères, 1 majuscule, 1 minuscule, 1 nombre et 1 caractère spécial</div>}
+            </div>
+            <div className="col">
+                <input name="passwordcheck" type="password" placeholder="Vérification mot de passe" ref={register({required: true, minLength: 8, pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,15}$/})}/>
+                {errors.passwordcheck && <div className="alert alert-danger" role="alert">le mot de passe doit contenir minimum 8 caractères, 1 majuscule, 1 minuscule, 1 nombre et 1 caractère spécial</div>}
+            </div>
+        </div>
+       <div className="row mb-4">
+        <div className="col">
+            <label>Je certifie avoir pris connaissance des règles d'utilisation</label>
+            <br/>
+            <input type="checkbox" name="check" aria-label="Checkbox for following text input" ref={register({required: true})}/>
+            {errors.check && <div className="alert alert-danger" role="alert">Il faut prendre connaissance des règles d'utilisation</div>}
+        </div>
+       </div>
+       <div className="form-group">
         <button className="btn btn-primary btn-lg" type="submit">Créer un compte</button>
       </div>
       <div className="form-group">
-        <button  type="submit" className="btn btn-primary btn-lg">J'ai déjà un compte, me connecter</button>
+        <button className="btn btn-primary btn-lg" type="submit">J'ai déjà uncompte, me connecter</button>
       </div>
       </form>
     </div>
