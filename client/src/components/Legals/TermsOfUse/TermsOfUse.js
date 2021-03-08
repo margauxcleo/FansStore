f09aef5c8c15b4bb3e15f15b5309740c9ef2cb1a
Senade Loko <lokosenade@gmail.com>
2021-03-08 20:34:16 +0100

    commit page validation commande et mentions légales

---
+++ client/src/components/Legals/TermsOfUse/TermsOfUse.js
@@ -0,0 +1,60 @@
+
+
+const TermsOfUse = () => {
+    return (
+        <>
+            <section class="bloc_page container-fluid" id="top">  
+        <div class="row">
+            <div class="website_infos offset-xs-1 col-xs-10 offset-sm-1 col-sm-10 offset-md-1 col-md-10 offset-xl-2 col-xl-8">
+                <h2 class="text-center">Mentions légales du site <span class="site_name">FansStore</span>
+                </h2>
+
+                <h3>Mentions légales</h3>
+                <p>
+                    <ul>
+                        <li>
+                            Editeur du site : Margaux VITEZ et Sènadé LOKO 
+                        </li>
+                        <li>
+                            Mail Margaux Vitez : margaux.vitez@hotmail.fr 
+                        </li>
+                        <li>
+                            Mail Sènadé Loko: lokosenade@gmail.com
+                        </li>
+                        <li>
+                            Directeur de la Publication : Margaux VITEZ et Sènadé LOKO
+                        </li>
+                        <li>
+                            Le nom de domaine <span class="site_name">fansstore.com</span> est la propriété des particuliers Margaux VITEZ et Sènadé LOKO.
+                        </li>
+                        <li>
+                            Hébergeur : HEROKU
+                        </li>
+                        <li>
+                            Création du webdesign : Margaux VITEZ et Sènadé LOKO
+                        </li>
+                    </ul>
+                </p>
+
+                <h3>Usage général</h3>
+                <p>
+                    Le site «fansstore.com » (le « Site ») est un site fictif créé dans le cadre d’une formation POEC pour devenir développeur front-end. Les présentes conditions d'utilisation ont pour objet de vous informer sur les conditions dans lesquelles vous pouvez accéder au Site et, le cas échéant, utiliser les données y figurant. 
+                </p>
+
+                <h3>Copyright (droit d'auteur)</h3>
+                <p>
+                    Tous les droits attachés aux copyrights restent la propriété du titulaire. 
+                </p>
+
+                <h3>Droit applicable</h3>	
+                <p>
+                    Le Site et les présentes conditions d'utilisation sont soumis aux dispositions du droit français.   Tout conflit relatif à l'utilisation du Site sera soumis à la compétence exclusive des tribunaux de Paris.
+                </p>		
+            </div>
+        </div>
+    </section>
+        </>
+    )
+}
+
+export default TermsOfUse;
---
+++ client/src/components/Order/OrderConfirmation/OrderConfirmation.js
@@ -0,0 +1,16 @@
+
+const OrderConfirmation = () => {
+    return (
+        <>
+            <p>
+                Votre commande n° ... est validée ! <br/>
+                Nous allons l'expédier sous 24h !
+                Nous vous remercions pour votre <br/>
+                confiance !
+                A très bientôt !
+            </p>
+        </>
+    )
+}
+
+export default OrderConfirmation;
