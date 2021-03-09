e2e8abd717c2eac0fc85ee7696d0d9baef1248c3
Margaux Vitez <margaux.vitez@hotmail.fr>
2021-03-09 15:54:36 +0100

    [MV]V.1.6 config heroku bdd (commentée)

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -13,3 +13,19 @@ module.exports = {
         idle: 10000
     }
 };
+
+// module.exports = {
+
+//     HOST: "d3y0lbg7abxmbuoi.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+//     USER: "k5na8f9i5padkjvz",
+//     PASSWORD: "c30wdgs6siwo47od",
+//     DB: "hjcauubt3kmy7cjw",
+//     dialect: "mysql",
+
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
